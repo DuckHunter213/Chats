519f92502f7920a6b825a8c7e0d832cf7dd1c57a
Gerardo0579 <gerardo0579@hotmail.com>
2016-09-27 13:59:06 -0500

    Reubica una clase

--- src/Cliente/cliente/ClienteUsuariosActivosPrivados.java
+++ src/Cliente/cliente/ClienteUsuariosActivosPrivados.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Interfaces;
+package Cliente.cliente;
 
+import Interfaces.usuariosActivosChatsPrivados;
 import cliente.interfaces.ListaUsuariosActivosChats;
 import java.rmi.RemoteException;
 
